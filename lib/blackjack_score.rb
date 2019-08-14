65494354cd418d52470ce05330e550e8b512f4ac
Emily Vomacka <emilyvomacka@gmail.com>
2019-08-13 22:17:20 -0700

    methods added to pass first three tests

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -3,5 +3,17 @@
 VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
 def blackjack_score(hand)
-
-end
+  score = 0
+  hand.each do |card|
+    if card.class == Integer 
+      score += card
+    elsif card == 'Jack' || card == 'Queen' || card == 'King'
+      score += 10
+    elsif card == 'Ace' && score < 11
+      score += 11
+    else 
+      score += 1
+    end 
+  end
+  return score 
+end 
--- test/blackjack_score_test.rb
+++ test/blackjack_score_test.rb
@@ -10,34 +10,48 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'Blackjack Score' do
   it 'can calculate the score for a pair of number cards' do
-
+    
     # Arrange
     hand = [3, 4]
-
+    
     # Act
     score = blackjack_score(hand)
-
-    # Assert <-  You do this part!
-
+    
+    # Assert
+    expect(score).must_equal 7
   end
-
+  
   it 'facecards have values calculated correctly' do
-
+    # Arrange
+    hand = ['Queen', 'King']
+    
+    # Act
+    score = blackjack_score(hand)
+    
+    # Assert
+    expect(score).must_equal 20
   end
-
+  
   it 'calculates aces as 11 where it does not go over 21' do
-
+    # Arrange
+    hand = [3, 'Ace']
+    
+    # Act
+    score = blackjack_score(hand)
+    
+    # Assert
+    expect(score).must_equal 14
   end
-
+  
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
-
+    
   end
-
+  
   it 'raises an ArgumentError for invalid cards' do
-
+    
   end
-
+  
   it 'raises an ArgumentError for scores over 21' do
-
+    
   end
 end
